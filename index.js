9de26c6804d51b6fd63ebac2a046911c65f2c5c6
aleksarad <alexa.radevich1@gmail.com>
2020-06-30 13:03:17 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let winOrNah = undefined
+
+  arr.find(function(record){
+    if(record.result === "W") {
+      winOrNah = record.year
+    }
+  })
+  
+  return winOrNah
+}
